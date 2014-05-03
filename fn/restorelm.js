dfe9e33ec6598a899dd44cbafbb6d3aaf6bae1a4
ttrfstud <ttrfstud@gmail.com>
2014-05-03 23:57:10 +0300

    not tested yet. functions are written

--- fn/restorelm.js
+++ fn/restorelm.js
@@ -1,17 +1,19 @@
+var direction = require('../obj/direction');
+
 function restorelm(H, T) {
   var len1, len2;
   var i, j;
   var max;
   var maxi, maxj;
 
-  var res;
+  var walk;
 
   max = -1;
 
   len1 = H.length;
   len2 = H[0].length;
 
-  res = [];
+  walk = [];
 
   for (i = 0; i < len1; i++) {
     for (j = 0; j < len2; j++) {
@@ -23,11 +25,27 @@ function restorelm(H, T) {
     }
   }
 
-  for (i = maxi; i >= 0; i--) {
-    for (j = maxj; j >= 0; j--) {
-      
+
+  for (i = maxi, j = maxj; i > 0 && j > 0;) {
+    walk.push({ i: i - 1, j: j - 1 });
+
+    switch(T[i][j]) {
+      case direction.diag:
+        i--;
+        j--;
+        break;
+      case direction.left:
+        j--;
+        break;
+      case direction.up:
+        i--;
+        break;
+      default:
+        break;
     }
   }
+
+  return walk;
 }
 
 module.exports = restorelm;
